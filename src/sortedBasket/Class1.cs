3e1036ceacb8a9c9215130965c87da27777f92ea
bobski009 <armand.lawson@xercise4less.co.uk>
2020-03-08 14:58:43 +0000

    Initial create of solution and projects

---
+++ src/sortedBasket/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sortedBasket
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/sortedCheckout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sortedCheckout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
