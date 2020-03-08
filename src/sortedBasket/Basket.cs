f5f6b93693651054cc105ac2f13df5e9a10b0ebc
bobski009 <armand.lawson@xercise4less.co.uk>
2020-03-08 16:30:27 +0000

    Added special offers and offers test now passes

--- src/sortedBasket/Basket.cs
+++ src/sortedBasket/Basket.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace sortedBasket
 {
     public class Basket
     {
-        private List<Item> internalBasket = new List<Item>();
+        private List<Item> _internalBasket = new List<Item>();
+        private List<Offers> _offers;
+
+        public Basket()
+        {
+            _offers = new List<Offers>
+            {
+                new Offers { Item = new Item { Sku = "A99", UnitPrice = 0.5m }, OfferMultiplier = 3, OfferPrice = 1.3m },
+                new Offers { Item = new Item { Sku = "B15", UnitPrice = 0.3m }, OfferMultiplier = 2, OfferPrice = 0.45m }
+            };
+
+        }
 
         public bool Scan(Item item)
         {
-            internalBasket.Add(item);
+            _internalBasket.Add(item);
             return true;
         }
 
@@ -17,9 +29,19 @@ namespace sortedBasket
         {
             var total = 0m;
 
-            foreach (var item in internalBasket)
+            foreach (var item in _internalBasket)
             {
                 total += item.UnitPrice;
+            }
+
+            foreach (var offer in _offers)
+            {
+                if (_internalBasket.Count(x => x.Sku == offer.Item.Sku) >= offer.OfferMultiplier)
+                {
+                    var qualifyingItems = _internalBasket.Count(x => x.Sku == offer.Item.Sku) / offer.OfferMultiplier;
+                    total = total - offer.Item.UnitPrice * offer.OfferMultiplier;
+                    total += offer.OfferPrice * qualifyingItems;
+                }
             }
 
             return total;
---
+++ src/sortedBasket/Offers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortedBasket
+{
+    public class Offers
+    {
+        public Item Item { get; set; }
+        public int OfferMultiplier { get; set; }
+        public decimal OfferPrice { get; set; }
+    }
+}
